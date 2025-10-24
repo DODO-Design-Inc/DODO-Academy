39ba9ec8f465d16babfbd2fb694c780a9a78da7a
mzmarshall1278 <mzmarshall1278@gmail.com>
2025-10-24 14:43:39 +0100

    add curriculum

---
+++ app/components/Curriculum.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function Curriculum() {
+  return (
+    <section className="mt-20 pt-20 bg-white text-[#1E1E1E]">
+        <div className=" text-center">
+            <h1 className="text-5xl font-normal leading-[100%] tracking-[-2px] mb-6">Complete course curriculum</h1>
+            <p className="w-[413px] mx-auto text-lg leading-[1.4] tracking-[-2%]">This user research course has been created and validated by experts in Design and User Research.</p>
+        </div>
+
+        <div className="flex flex-wrap gap-4 justify-center pb-[158px] mt-16">
+            <div className="w-[649px] bg-[#F9FAFB] pt-10 pb-[42px] pl-[38.5px] rounded">
+                <h5 className='text-lg leading-[1.4] tracking-[-2%] font-medium text-[#616569] mb-2'>WEEK 1</h5>
+                <p className="font-medium text-[25px] leading-[1.23] tracking-[-2%] text-[#1E1E1E] w-[309px]">Foundational Principles of User and Design Research
+                </p>
+            </div>
+
+            <div className="w-[649px] bg-[#F9FAFB] pt-10 pb-[42px] pl-[38.5px] rounded">
+                <h5 className='text-lg leading-[1.4] tracking-[-2%] font-medium text-[#616569] mb-2'>WEEK 2</h5>
+                <p className="font-medium text-[25px] leading-[1.23] tracking-[-2%] text-[#1E1E1E] w-[377px]">Core Research Methods for Generating Actionable Insights
+                </p>
+            </div>
+
+            <div className="w-[649px] bg-[#F9FAFB] pt-10 pb-[42px] pl-[38.5px] rounded">
+                <h5 className='text-lg leading-[1.4] tracking-[-2%] font-medium text-[#616569] mb-2'>WEEK 3</h5>
+                <p className="font-medium text-[25px] leading-[1.23] tracking-[-2%] text-[#1E1E1E] w-[429px]">The Role of Research in Product Design and Human-Centered Design
+                </p>
+            </div>
+
+            <div className="w-[649px] bg-[#F9FAFB] pt-10 pb-[42px] pl-[38.5px] rounded">
+                <h5 className='text-lg leading-[1.4] tracking-[-2%] font-medium text-[#616569] mb-2'>WEEK 4</h5>
+                <p className="font-medium text-[25px] leading-[1.23] tracking-[-2%] text-[#1E1E1E] w-[374px]">Analysis and Synthesis Techniques for Design Impact
+                </p>
+            </div>
+
+            <div className="w-[649px] bg-[#F9FAFB] pt-10 pb-[42px] pl-[38.5px] rounded">
+                <h5 className='text-lg leading-[1.4] tracking-[-2%] font-medium text-[#616569] mb-2'>WEEK 5</h5>
+                <p className="font-medium text-[25px] leading-[1.23] tracking-[-2%] text-[#1E1E1E] w-[370px]">Learning Through Prototyping, Testing, and Iterative Design
+                </p>
+            </div>
+
+            <div className="w-[649px] bg-[#F9FAFB] pt-10 pb-[42px] pl-[38.5px] rounded">
+                <h5 className='text-lg leading-[1.4] tracking-[-2%] font-medium text-[#616569] mb-2'>WEEK 6</h5>
+                <p className="font-medium text-[25px] leading-[1.23] tracking-[-2%] text-[#1E1E1E] w-[450px]">Storytelling, Communicating Insights and Influencing Stakeholders
+                </p>
+            </div>
+        </div>
+
+    </section>
+  )
+}
